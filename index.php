84837fc54670ced7879e6276d619eee07d06dc65
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-02-22 13:27:07 +0700

    set up index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-    echo "testing";
+    echo "Selamat datang di pesan sego \n";
+    $opsi = ["Tambah pesanan","Bayar","Lihat isi pesanan","Keluar"];
+    while(true) {
+        foreach($opsi as $key=>&$value) {
+            $key++;
+            echo  "$key. $value \n";
+        }
+        echo "Masukkan layanan yang ingin kamu gunakan : ";
+        $layanan = trim(fgets(STDIN));
+    }
 ?>
